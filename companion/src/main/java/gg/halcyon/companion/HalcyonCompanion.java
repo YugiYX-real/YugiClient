7de31c903321ae5bc0203fdc7d8585739e7c4bee
YugiYX-real <yukiyx85@gmail.com>
2026-08-06 17:30:40 +0200

    feat(companion): add shared companion constants

---
+++ companion/src/main/java/gg/halcyon/companion/HalcyonCompanion.java
@@ -0,0 +1,13 @@
+package gg.halcyon.companion;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/** Shared constants for the Halcyon companion mod. */
+public final class HalcyonCompanion {
+	public static final String MOD_ID = "halcyon";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger("Halcyon");
+
+	private HalcyonCompanion() {}
+}
